d70fc73e7c94d22f781c276eef412eec38b84062
Zauberhut <49160827+Zauberhut@users.noreply.github.com>
2022-09-28 10:33:31 +0200

    Zeige Kreuz

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ function Serpentine_Matrix_xyRGB (x: number, y: number, R: number, G: number, B:
     if (y % 2 == 1) {
         strip.setMatrixColor(x, y, neopixel.rgb(R, G, B))
     } else {
-        strip.setMatrixColor(Matrixbreite + 1 - x, y, neopixel.rgb(R, G, B))
+        strip.setMatrixColor(Matrixbreite - 1 - x, y, neopixel.rgb(R, G, B))
     }
 }
 let strip: neopixel.Strip = null
@@ -11,6 +11,9 @@ Matrixbreite = 8
 strip = neopixel.create(DigitalPin.P0, 64, NeoPixelMode.RGB)
 strip.setMatrixWidth(Matrixbreite)
 basic.forever(function () {
-    Serpentine_Matrix_xyRGB(3, 4, 0, 20, 255)
-    strip.show()
+    for (let Index = 0; Index <= 7; Index++) {
+        Serpentine_Matrix_xyRGB(Index, Matrixbreite - 1 - Index, 0, 20, 200)
+        Serpentine_Matrix_xyRGB(Index, Index, 200, 20, 0)
+        strip.show()
+    }
 })
