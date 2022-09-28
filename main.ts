86c2311f8a091a8038e965c7503f7e76542b70ea
Zauberhut <49160827+Zauberhut@users.noreply.github.com>
2022-09-28 10:53:37 +0200

    added x

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+function Serpentine_x (x: number) {
+    if (0 % 2 == 1) {
+    	
+    } else {
+        strip.setMatrixColor(Matrixbreite - 1 - x, 0, neopixel.rgb(0, 0, 0))
+    }
+}
 function Serpentine_Matrix_xyRGB (x: number, y: number, R: number, G: number, B: number) {
     if (y % 2 == 1) {
         strip.setMatrixColor(x, y, neopixel.rgb(R, G, B))
@@ -12,6 +19,7 @@ strip = neopixel.create(DigitalPin.P0, 64, NeoPixelMode.RGB)
 strip.setMatrixWidth(Matrixbreite)
 basic.forever(function () {
     for (let Index = 0; Index <= 7; Index++) {
+        strip.setMatrixColor(0, 0, neopixel.colors(NeoPixelColors.Red))
         Serpentine_Matrix_xyRGB(Index, Matrixbreite - 1 - Index, 0, 20, 200)
         Serpentine_Matrix_xyRGB(Index, Index, 200, 20, 0)
         strip.show()
